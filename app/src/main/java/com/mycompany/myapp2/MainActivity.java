d31c91ce6e3cdc409ce40048679447dd6a8560ec
869827799@qq.com <869827799@qq.com>
2019-05-25 07:26:56 +0800

    a

--- app/src/main/java/com/mycompany/myapp2/MainActivity.java
+++ app/src/main/java/com/mycompany/myapp2/MainActivity.java
@@ -1,14 +1,40 @@
 package com.mycompany.myapp2;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity
+{
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //ProgressDialog.show(this, "标题", "内容");
+        final ProgressDialog progressDialog = new ProgressDialog(MainActivity.this);
+        progressDialog.setTitle("标题");
+        progressDialog.setMessage("提示信息");
+        progressDialog.setIcon(R.mipmap.ic_launcher);
+        progressDialog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
+        progressDialog.setCancelable(false); // 能够返回
+        progressDialog.setCanceledOnTouchOutside(false); // 点击外部返回
+        progressDialog.setMax(100);
+        progressDialog.incrementProgressBy(50);
+        //progressDialog.setIndeterminate(true);
+        progressDialog.setButton(ProgressDialog.BUTTON_NEGATIVE, "取消", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    progressDialog.dismiss();
+                }
+            });
+            
+            
+        progressDialog.show();
+        
     }
-    
+
 }
